0572511534270e9c74c436284b451dd2999914d4
25MAGA25 <maga250305125@gmail.com>
2024-11-24 21:08:19 +0600

    com6

--- homework/hw_4.py
+++ homework/hw_4.py
@@ -2,3 +2,7 @@ import numpy as np
 from bashplotlib.histogram import plot_hist
 arr = np.random.normal(size=1000, loc=0, scale=1)
 plot_hist(arr, bincount=50)
+
+# bashplotlib — это пакет python и инструмент командной
+# строки для создания базовых графиков в терминале.
+# Это быстрый способ визуализации данных, когда у вас нет графического интерфейса
